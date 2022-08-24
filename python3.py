2689591b5596bd1f76ca9ec5a08ef8c3c766e343
Shiva Krishna <110969816+YSK97@users.noreply.github.com>
2022-08-24 21:27:30 +0530

    Add files via upload
    PYTHON PRACTICE PROGRAMS

---
+++ python3.py
@@ -0,0 +1,23 @@
+a=int(input('enter first number'))
+b=int(input('enter second number'))
+c=a+b
+print(c)
+print("sum of {}+{}={}".format(a,b,c))
+a=int(input('enter first number'))
+b=int(input('enter second number'))
+c=a-b
+print(c)
+print("difference of {}-{}={}".format(a,b,c))
+a=int(input('enter first number'))
+b=int(input('enter second number'))
+c=a*b
+print(c)
+print("product of {}*{}={}".format(a,b,c))
+a=int(input('enter first number'))
+b=int(input('enter second number'))
+c=a/b
+print(c)
+print("division of {}/{}={}".format(a,b,c))
+
+
+
